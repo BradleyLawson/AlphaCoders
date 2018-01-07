7c306eed56d60dcabce056fb17e3e2844c0ff129
Brad Lawson <bradlawson@cfl.rr.com>
2018-01-07 13:36:20 -0500

    initial commit with HTML

---
+++ assets/javascript/gotCoinJS.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC_iqQW_F3errT4mRYM2IN0KDZAhx3U-hg",
+    authDomain: "gotcoin-db44a.firebaseapp.com",
+    databaseURL: "https://gotcoin-db44a.firebaseio.com",
+    projectId: "gotcoin-db44a",
+    storageBucket: "",
+    messagingSenderId: "492118808139"
+  };
+  firebase.initializeApp(config);
+</script>
